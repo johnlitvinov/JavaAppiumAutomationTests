3a1906dc7e540720c21f5958f380654662d3e360
IvanLitvinov <ivan.litvinau@appodeal.com>
2021-03-22 15:34:39 +0300

    added ex6 task

---
+++ src/Ex6task.java
@@ -0,0 +1,101 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.net.URL;
+
+
+public class Ex6task {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Pixel");
+        capabilities.setCapability("platformVersion", "11");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "/Users/apple/Desktop/JavaAppiumAutomationTests/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+
+    @Test
+    public void ex6() {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Can't find text 'Search Wikipedia'",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Kotlin",
+                "",
+                10
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Kotline')]"),
+                "Can't find 'Kotline'",
+                10
+        );
+
+        String search_title = "Kotline";
+
+        assertElementPresent(
+                By.xpath("//*[contains(@text,'Kotline')]"),
+                "text",
+                search_title,
+                "The text does not contain " + search_title ,
+                5
+        );
+    }
+
+    private void assertElementPresent(By by, String attributeName, String expectedText, String error_message,long timeoutSeconds) {
+        WebElement element = waitForElementPresent(by, error_message,timeoutSeconds);
+        Assert.assertEquals(
+                "Titles are not equals",
+                expectedText,
+                element.getAttribute(attributeName)
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+}//end class
+
